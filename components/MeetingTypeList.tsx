7f573c72158d9f31158e383d5397c82984f6f76e
jps27CSE <jackpritomsoren@hotmail.com>
2024-10-13 13:00:28 +0600

    Reusable Modal (Complete)

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -2,6 +2,7 @@
 import HomeCard from "@/components/HomeCard";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import MeetingModal from "@/components/MeetingModal";
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -9,13 +10,15 @@ const MeetingTypeList = () => {
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
 
+  const CreateMeeting = () => {};
+
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
         img="/icons/add-meeting.svg"
         title="New Meeting"
         description="Start an instant meeting"
-        handleClick={() => setMeetingState("isJoiningMeeting")}
+        handleClick={() => setMeetingState("isInstantMeeting")}
         className="bg-orange-1"
       />
       <HomeCard
@@ -29,7 +32,7 @@ const MeetingTypeList = () => {
         img="/icons/recordings.svg"
         title="View Recordings"
         description="Check out your recordings"
-        handleClick={() => setMeetingState("isJoiningMeeting")}
+        handleClick={() => router.push("/recordings")}
         className="bg-purple-1"
       />
       <HomeCard
@@ -38,6 +41,15 @@ const MeetingTypeList = () => {
         description="Via invitation link"
         handleClick={() => setMeetingState("isJoiningMeeting")}
         className="bg-yellow-1"
+      />
+
+      <MeetingModal
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={CreateMeeting}
       />
     </section>
   );
