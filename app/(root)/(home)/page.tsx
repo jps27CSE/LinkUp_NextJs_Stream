47dae7c434f7e52ae9cab419a247ec258e0b4b64
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2024-11-04 15:46:08 +0600

    Fixed home component time date

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -7,6 +7,13 @@ const Home = () => {
   const { upcomingCalls } = useGetCalls();
   // @ts-ignore
   const now = new Date();
+  const time = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+    now,
+  );
 
   const nextMeeting = upcomingCalls.length > 0 ? upcomingCalls[0] : null;
 
@@ -36,12 +43,8 @@ const Home = () => {
             Upcoming Meeting on: {formattedDate} at {formattedTime}
           </h2>
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl">
-              {formattedTime}
-            </h1>
-            <p className="text-lg font-medium text-sky-1 lg:text-2xl">
-              {formattedDate}
-            </p>
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
           </div>
         </div>
       </div>
